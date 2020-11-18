a7ec944ed6de65046c3e7c55e561d1ca62203896
yuvisharma7 <71940915+yuvisharma7@users.noreply.github.com>
2020-11-17 20:52:39 -0600

    Add files via upload

---
+++ box.js
@@ -0,0 +1,21 @@
+class Box{
+    block1= new Block(330,235,30,40);
+    block2= new Block(360,235,30,40);
+    block3= new Block(390,235,30,40);
+    block4= new Block(420,235,30,40);
+    block5= new Block(450,235,30,40);
+    
+    block6= new Block(330,195,30,40);
+    block7= new Block(330,195,30,40);
+    block8= new Block(330,1955,30,40);
+    
+    block9= new Block(330,155,30,40);
+
+     
+
+
+
+
+
+
+}
